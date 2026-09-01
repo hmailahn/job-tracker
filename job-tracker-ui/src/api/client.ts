ba23afc1f9b1e33de28b40698cebf8c09319d78d
Heidi <mailahnheidi@gmail.com>
2026-09-01 16:00:42 -0500

    add backend api route

--- job-tracker-ui/src/api/client.ts
+++ job-tracker-ui/src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://job-tracker-production-06bb.up.railway.app/api',
 });
 
 client.interceptors.request.use((config) => {
