befbcb3ca613c9919d864eeeb220c6929b362053
Heidi <mailahnheidi@gmail.com>
2026-09-02 10:02:43 -0500

    fix login

--- job-tracker-ui/src/api/client.ts
+++ job-tracker-ui/src/api/client.ts
@@ -15,7 +15,8 @@ client.interceptors.request.use((config) => {
 client.interceptors.response.use(
   (response) => response,
   (error) => {
-    if (error.response?.status === 401) {
+    const hadToken = !!localStorage.getItem('token');
+    if (error.response?.status === 401 && hadToken) {
       localStorage.removeItem('token');
       window.location.href = '/login';
     }
